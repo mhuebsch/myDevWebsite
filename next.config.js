0dd28b2c38fab93b001d9d53b55a22e86a747255
mhuebsch <mhipsh@gmail.com>
2024-09-25 09:06:13 -0400

    change exports from commonJS to ES modules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   reactStrictMode: false,
   webpack(config) {
     config.module.rules.push({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './app/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
