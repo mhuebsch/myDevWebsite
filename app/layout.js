f2844344e019cfc2581777670b410fc04b095d72
mhuebsch <mhipsh@gmail.com>
2024-10-09 12:11:32 -0400

    update metadata, site description, add openGraph tags, site preview image

--- app/layout.js
+++ app/layout.js
@@ -7,16 +7,48 @@ import NavigationHamburger from './components/navigationHamburger.tsx';
 import Particles from './components/particles';
 
 export const metadata = {
-  title: 'Mark Huebsch | Designer | Developer | Engineer',
-  description: 'Mark Huebsch is an experienced front end specialist.',
+  title: 'Mark Huebsch | Developer | Designer | Engineer',
+  description:
+    'Mark Huebsch is a seasoned web developer and designer, specializing in creating dynamic, user-focused digital experiences. With over 15 years of expertise in front-end development and UI/UX design, Mark delivers innovative solutions that bridge design and technology.',
   icons: {
     icon: '/favicon.png',
+  },
+  openGraph: {
+    title: 'Mark Huebsch | Designer | Developer | Engineer',
+    description:
+      'Mark Huebsch is a seasoned web developer and designer, specializing in creating dynamic, user-focused digital experiences. With over 15 years of expertise in front-end development and UI/UX design, Mark delivers innovative solutions that bridge design and technology.',
+    url: 'https://dev.markhuebsch.com',
+    images: [
+      {
+        url: '/images/website-preview-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Mark Huebsch website preview image',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    image: '/images/website-preview-image.png',
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-black">
+      <head>
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:image" content={metadata.twitter.image} />
+      </head>
       <body>
         <NavigationHamburger className="fixed" />
         <Particles
