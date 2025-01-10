79ced2d91351af5a7af78078e71e066785a11c54
mhuebsch <mhipsh@gmail.com>
2025-01-10 11:22:02 -0500

    add console logs to middleware for debugging

--- middleware.js
+++ middleware.js
@@ -1,18 +1,35 @@
 import { NextResponse } from 'next/server';
 
+// export function middleware(request) {
+//   const authenticated = request.cookies.get('authenticated');
+
+//   if (authenticated !== 'true') {
+//     // Redirect to a page or show a login form if not authenticated
+//     return NextResponse.redirect(new URL('/login', request.url));
+//   }
+
+//   // Allow access to the page
+//   return NextResponse.next();
+// }
+
+// // Limit middleware to specific routes
+// export const config = {
+//   matcher: '/projects/interactive-presentations', // Protect this route
+// };
+
 export function middleware(request) {
   const authenticated = request.cookies.get('authenticated');
+  console.log('Middleware: Authenticated Cookie Value:', authenticated); // Log the cookie
 
   if (authenticated !== 'true') {
-    // Redirect to a page or show a login form if not authenticated
+    console.log('Middleware: Redirecting to /login'); // Log the redirect
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  // Allow access to the page
+  console.log('Middleware: Allowing access'); // Log allowed access
   return NextResponse.next();
 }
 
-// Limit middleware to specific routes
 export const config = {
-  matcher: '/projects/interactive-presentations', // Protect this route
+  matcher: '/projects/interactive-presentations',
 };
