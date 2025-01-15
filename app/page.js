9d1569316cda20bba8d8620459c649e2c335d384
mhuebsch <mhipsh@gmail.com>
2025-01-15 13:14:40 -0500

    homepage mobile styling

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ const listItemClassName = 'mb-1 sm:mb-8 mx-2';
 
 function Header({ title }) {
   return (
-    <header className="text-green-300 my-8 sm:my-16 uppercase text-center animate-fade-in-delayed tracking-wide">
+    <header className="order-2 sm:order-none text-green-300 mt-2 sm:mt-8 my-8 sm:my-16 uppercase text-center animate-fade-in-delayed tracking-wide">
       <h1 className="text-xl">{title ? title : 'Default title'}</h1>
     </header>
   );
@@ -15,7 +15,7 @@ function Header({ title }) {
 export default function HomePage() {
   return (
     <div className="flex mt-16 sm:mt-0 flex-col md:flex-row items-center justify-center w-screen px-10 max-w-full sm:max-w-[70%] lg:max-w-[1200px] mx-auto h-screen">
-      <div className="sm:w-full md:w-3/12 mt-24 sm:mt-0 flex flex-col items-center justify-center animate-fade-in-delayed">
+      <div className="px-6 mt-52 sm:w-full md:w-3/12 md:mt-0  flex flex-col items-center justify-center animate-fade-in-delayed">
         <img
           src="/images/mark-headshot-nobg.png"
           alt="headshot profile picture of mark huebsch"
@@ -26,11 +26,11 @@ export default function HomePage() {
       <div className="sm:w-full md:w-9/12 flex flex-col items-center justify-center pb-16 sm:pb-0">
         <Header title="Frontend Developer" />
 
-        <h1 className="font-semibold lowercase py-3.5 px-0.5 z-10 text-3xl text-transparent duration-1000 bg-green-800 cursor-default text-edge-outline animate-title font-display sm:text-6xl xl:text-8xl whitespace-nowrap bg-clip-text ">
+        <h1 className="order-1 sm:order-none font-semibold lowercase mt-4 sm:mt-0 py-3.5 px-0.5 z-10 text-3xl text-transparent duration-1000 bg-green-800 cursor-default text-edge-outline animate-title font-display sm:text-6xl xl:text-8xl whitespace-nowrap bg-clip-text ">
           mark huebsch
         </h1>
 
-        <nav className="home-nav-links my-4 sm:my-16 animate-fade-in-delayed w-full max-w-screen-sm sm:max-w-prose">
+        <nav className="order-3 sm:order-none home-nav-links my-4 sm:my-16 animate-fade-in-delayed w-full max-w-screen-sm sm:max-w-prose">
           <ul className="flex flex-col flex-wrap sm:flex-row items-center justify-center gap-10 sm:gap-4 max-w-screen-sm">
             <li className={listItemClassName}>
               <Link href="/projects" className={linkClassName}>
