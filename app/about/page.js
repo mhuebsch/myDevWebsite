ea53671a63e3b7ed0341b0ea6a929f4509470e8c
mhuebsch <mhipsh@gmail.com>
2025-01-31 15:39:31 -0500

    change wording on about page

--- app/about/page.js
+++ app/about/page.js
@@ -17,7 +17,7 @@ const AboutPage = () => {
           <p className="w-full sm:w-2/5 px-8 sm:px-0 text-center">
             I reside in the mountains in Asheville, North Carolina and enjoy
             outdoor activities in my free time. When I'm not coding or
-            designing, you'll often find me hiking, camping, or spending time on
+            designing, you can find me hiking, camping, or spending time on
             a lake or river.
           </p>
         </div>
